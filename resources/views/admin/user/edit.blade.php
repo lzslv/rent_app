f8918767212e14b7211bf49381787e52b9aa2b91
VladislavPanda <pepartvlad7@gmail.com>
2024-04-13 23:13:55 +0300

    Added styles for show and update views

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.admin')
 @section('content')
-    <h3>Редактировать</h3>
+    <div class="ml-3 mr-3">
+        <h3>Редактировать</h3>
 
-    <form action="{{route('admin.user.update', $user)}}" method="post">
-        @csrf
-        @method('patch')
-        <div class="mb-3">
-            <label for="name" class="form-label">Имя</label>
-            <input type="text" name="name" class="form-control" id="name" value="{{$user->name}}">
-        </div>
+        <form action="{{route('admin.user.update', $user)}}" method="post">
+            @csrf
+            @method('patch')
+            <div class="mb-3">
+                <label for="name" class="form-label">Имя</label>
+                <input type="text" name="name" class="form-control" id="name" value="{{$user->name}}">
+            </div>
 
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" name="email" class="form-control" id="email" value="{{$user->email}}">
-        </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email" value="{{$user->email}}">
+            </div>
 
-        <div class="mb-3">
-            <label for="password" class="form-label">Пароль</label>
-            <input type="password" name="password" class="form-control" id="password" value="{{$user->password}}">
-        </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Пароль</label>
+                <input type="password" name="password" class="form-control" id="password" value="{{$user->password}}">
+            </div>
 
-        <div class="mb-3">
-            <label for="role" class="form-label">Роль</label>
-            <select name="role" class="form-control">
-                @foreach($roles as  $id => $role)
-                    <option value="{{$id}}"
-                        {{$id = $user->role ? ' selected' : ''}}>
-                        {{$role}}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Обновить</button>
-    </form>
-
+            <div class="mb-3">
+                <label for="role" class="form-label">Роль</label>
+                <select name="role" class="form-control">
+                    @foreach($roles as  $id => $role)
+                        <option value="{{$id}}"
+                            {{$id = $user->role ? ' selected' : ''}}>
+                            {{$role}}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Обновить</button>
+        </form>
+    </div>
 @endsection
