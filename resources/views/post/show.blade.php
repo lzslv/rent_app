0e83a8c068c699eca6fba6d959bc36368e7b1697
VladislavPanda <pepartvlad7@gmail.com>
2024-05-08 01:37:50 +0300

    Added validation for dates input

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -145,4 +145,16 @@
             </div>
         </div>
     </div>
+    <script>
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+        let day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate();
+        let hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
+        let minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
+
+        let minDateTime = year + '-' + month + '-' + day + 'T' + hours + ':' + minutes;
+
+        document.getElementById('date').min = minDateTime;
+    </script>
 @endsection
